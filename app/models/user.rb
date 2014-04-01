b9f96200ebe049ca124358189111dc2813c98d99
nplutzik <nplutzik@gmail.com>
2014-04-01 16:33:46 -0400

    updated files

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,33 @@
-class User < ActiveRecord::Base
-  has_secure_password
-  has_many(:favorites)
-  has_many(:services, through: :favorites)
-  has_many(:favorites)
+class User
+require 'rubygems'
+require 'data_mapper'
 
-  # validates(:email, uniqueness: true, presence: true)
-  # # validates(:password, length: { minimum: 5 }, on: :create)
-  # validates(:name, presence: true)
-  # validates(:admin, inclusion: { in: [true, false] })
-  # validates(:balance, numericality: {
-  #                           only_integer: true,
-  #                           greater_than_or_equal_to: 0 })
+DataMapper.setup(:default, 'postgres://nplutzik:password@localhost/database')
+
+  include DataMapper::Resource
+    equivalence table =>
+    has_secure_password
+    has_n(:favorites)
+    has_n(:services, :through => user_favorites
+
+
+
+  property :id,     Serial, :key => true
+  property :name,  String, :required => true, :unique => true, :format => /[a-z]/
+  property :email,  String, :required => true, :unique => true, :format => /@/
+  property :information,  Text, :required => true, :unique => true, :format => /[a-z]/
+  property :password_digest,  String, :required => true, :unique => true, :format => /[a-z]/
+
+  property :created_at, DateTime
+  property :updated_at, DateTime
+
+  end
+
+    def self.authenticate(login, pass)
+        u = User.first(:login => login )
+        return nil if u.nil?
+        return u if u.password == pass
+     end
 
 end
+
