05e9aa3b70867c49372dbff10b9bfa65f6b0d41e
nplutzik <nplutzik@gmail.com>
2014-04-01 19:04:54 -0400

    updated files

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,3 +1,24 @@
-class Service < ActiveRecord::Base
+require 'rubygems'
+require 'data_mapper'
+DataMapper.setup(:default, 'postgres://nplutzik:@localhost/serveforcommunity_development')
 
+class Service
+  include DataMapper::Resource
+  has_secure_password
+  has n(:users)
+  belongs_to(:user)
+
+
+  property :name,    Serial, :key => true
+  property :address,  String, :required => true, :unique => true, :format => /[a-z]/
+  property :information, Text, :required => true, :unique => true, :format => /[a-z]/
+  property :password_digest,  String, :required => true, :format => /[a-z]/
+
+   def self.authenticate(login, pass)
+        u = User.first(:login => login )
+        return nil if u.nil?
+        return u if u.password == pass
+     end
 end
+DataMapper.finalize
+DataMapper.auto_upgrade!
