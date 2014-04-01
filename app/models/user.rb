9822135cb7eb16cda49b4668375d21843a2a9c2f
nplutzik <nplutzik@gmail.com>
2014-04-01 17:20:12 -0400

    updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User
   property :id,    Serial, :key => true
   property :name,  String, :required => true, :unique => true, :format => /[a-z]/
   property :email, String, :required => true, :unique => true, :format => /@/
-  property :information,  Text, :required => true, :unique => true, :format => /[a-z]/
-  property :password_digest,  String, :required => true, :unique => true, :format => /[a-z]/
+  property :information,  Text, :required => true
+  property :password_digest,  String, :required => true
 
   property :created_at, DateTime
   property :updated_at, DateTime
